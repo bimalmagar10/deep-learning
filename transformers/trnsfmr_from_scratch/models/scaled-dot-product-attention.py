f42b219e42dcc1264d95c59d589ee40c100fa836
bimalmagar10 <bimalmagar873@gmail.com>
2025-08-03 23:06:45 -0500

    progress: initialized scaled dot product attention

--- transformers/trnsfmr_from_scratch/models/scaled-dot-product-attention.py
+++ transformers/trnsfmr_from_scratch/models/scaled-dot-product-attention.py
@@ -1,3 +1,12 @@
 import torch.nn as nn
 
-# class ScaledDotProductAttention(nn.Module):
+# Scaled Dot Product Attention Module from transformers
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0.1):
+        super()
+        self.w_q = None # Query weights
+        self.w_k = None # Key weights
+        self.w_v = None # Vaue weights
+
+    def forward(self,query,key,value):
+        pass
